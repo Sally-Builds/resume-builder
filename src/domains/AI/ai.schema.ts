41091b64c19374df7d13f2828a3fb1fba6980d9a
Joshua Uzoagulu <joshua.uzoagulu@netapps.com.ng>
2024-12-19 16:59:48 +0100

    email change

--- src/domains/AI/ai.schema.ts
+++ src/domains/AI/ai.schema.ts
@@ -76,7 +76,7 @@ export const AdditionalInformationSchema = z.object({
 export const ResumeSchema = z.object({
     name: z.string(),
     address: z.string(),
-    email: z.string().trim().email(),
+    email: z.string().transform(str => str.replace(/\s+/g, '')).email(),
     professionalSummary: z.object({
         section_title: z.string(),
         content: z.string()
